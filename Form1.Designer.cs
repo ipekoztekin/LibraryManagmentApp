0095b57ff42e1f69b5f0355c48ad92ab106adfa0
ipekoztekin <138534154+ipekoztekin@users.noreply.github.com>
2024-04-08 01:04:11 +0300

    First

---
+++ Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace LibraryManagmentSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.add = new System.Windows.Forms.Button();
+            this.delete = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 280);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Book Title:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 313);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Author:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 347);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "ISBN:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.FloralWhite;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(545, 235);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FloralWhite;
+            this.textBox1.Location = new System.Drawing.Point(140, 281);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(212, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FloralWhite;
+            this.textBox2.Location = new System.Drawing.Point(140, 313);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(212, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.FloralWhite;
+            this.textBox3.Location = new System.Drawing.Point(140, 344);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(212, 22);
+            this.textBox3.TabIndex = 6;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.FloralWhite;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Available",
+            "Occupied"});
+            this.comboBox1.Location = new System.Drawing.Point(140, 382);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(212, 24);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(50, 382);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Availability:";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FloralWhite;
+            this.button1.Location = new System.Drawing.Point(633, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 32);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Listing";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // add
+            // 
+            this.add.BackColor = System.Drawing.Color.FloralWhite;
+            this.add.Location = new System.Drawing.Point(633, 205);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(121, 33);
+            this.add.TabIndex = 10;
+            this.add.Text = "To Add";
+            this.add.UseVisualStyleBackColor = false;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // delete
+            // 
+            this.delete.BackColor = System.Drawing.Color.FloralWhite;
+            this.delete.Location = new System.Drawing.Point(633, 128);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(121, 34);
+            this.delete.TabIndex = 11;
+            this.delete.Text = "Delete";
+            this.delete.UseVisualStyleBackColor = false;
+            this.delete.Click += new System.EventHandler(this.delete_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::LibraryManagmentSystem.Properties.Resources.owl;
+            this.pictureBox1.Location = new System.Drawing.Point(495, 356);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(53, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(554, 382);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(167, 20);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "İPEK ÖZTEKİN 222523074";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.label5.UseCompatibleTextRendering = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::LibraryManagmentSystem.Properties.Resources.magic_ball;
+            this.pictureBox2.Location = new System.Drawing.Point(573, 32);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(54, 48);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 14;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::LibraryManagmentSystem.Properties.Resources.coffin;
+            this.pictureBox3.Location = new System.Drawing.Point(576, 117);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(51, 45);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 15;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::LibraryManagmentSystem.Properties.Resources.magic_wand;
+            this.pictureBox4.Location = new System.Drawing.Point(576, 192);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(53, 46);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 16;
+            this.pictureBox4.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Tan;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.delete);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Library Managment";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Button delete;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Collections;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace LibraryManagmentSystem
+{
+    public partial class Form1 : Form
+    {
+
+        private string connectionString = "Server=DESKTOP-NFUGI3G\\SQLEXPRESS;Database=LibraryManagement;User Id=sa;Password=996633;";
+        public SqlCommand SqlCommand { get; private set; }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "SELECT * FROM Books";
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Open();
+
+                DataTable table = new DataTable();
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    table.Load(reader);
+                }
+                dataGridView1.DataSource = table;
+                connection.Close();
+            }
+        }
+
+        private void delete_Click(object sender, EventArgs e)
+        {
+            // Kullanıcı tarafından seçilen satırı alın
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                // Seçilen satırın indeksini alın
+                int rowIndex = dataGridView1.SelectedRows[0].Index;
+
+                // Veritabanından silinecek satırın kimliğini alın
+                int id = Convert.ToInt32(dataGridView1.Rows[rowIndex].Cells["BookId"].Value); // IDColumn, veritabanındaki benzersiz kimliği içeren sütun adıdır
+
+                // Veritabanından seçilen satırı kaldırın
+                VeritabanındanSil(id);
+
+                // DataGridView'i güncelleyin
+                button1_Click_1(sender, e);
+            }
+            else
+            {
+                MessageBox.Show("Lütfen Silmek İstediğiniz Kitabı Seçip Tekrar Deneyiniz.", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Veritabanından satırı silmek için bir yöntem
+        private void VeritabanındanSil(int id)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "DELETE FROM Books WHERE BookId = @BookId";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@BookId", id);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            // textbox1'deki metni al
+            string bookName = textBox1.Text;
+
+            // Ekleme sorgusunu oluştur
+            string query = "";
+
+            // Girişlerin boş olup olmadığını kontrol edin
+            if (string.IsNullOrWhiteSpace(textBox1.Text) ||
+                string.IsNullOrWhiteSpace(textBox2.Text) ||
+                string.IsNullOrWhiteSpace(textBox3.Text) ||
+                comboBox1.SelectedItem == null)
+            {
+                MessageBox.Show("Lütfen Eksik Bilgi Girmeyiniz Verilen Bilgi Kutucuklarını Kontrol Ediniz.", "HATA !", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return; // Eğer herhangi bir alan boş ise işlemi durdur
+            }
+            else
+            {
+                // ISBN'nin veritabanında mevcut olup olmadığını kontrol et
+                if (ISBNExists(textBox3.Text))
+                {
+                    MessageBox.Show("Girmiş Olunan Seri Numarasında Bir Kitap Zaten Mevcuttur. Bilgileri Tekrar Kontrol Ediniz.", "Mevcut", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+
+                query = "INSERT INTO Books (Title,Author,ISBN,Available) VALUES (@BookTitle,@BookAuthor,@BookISBN,@BookAvailable)";
+            }
+
+            // SqlConnection ve SqlCommand nesnelerini oluştur
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                // Parametre ekleyerek güvenli bir sorgu yapın
+                command.Parameters.AddWithValue("@BookTitle", textBox1.Text);
+                command.Parameters.AddWithValue("@BookAuthor", textBox2.Text);
+                command.Parameters.AddWithValue("@BookISBN", textBox3.Text);
+                command.Parameters.AddWithValue("@BookAvailable",comboBox1.Text);
+
+                // Bağlantıyı açın ve sorguyu çalıştırın
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+
+            // Ekleme işlemi tamamlandıktan sonra DataGridView'i güncelle
+            button1_Click_1(sender, e);
+        }
+
+       
+
+        // ISBN'nin veritabanında mevcut olup olmadığını kontrol et
+        private bool ISBNExists(string isbn)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "SELECT COUNT(*) FROM Books WHERE ISBN = @ISBN";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@ISBN", isbn);
+
+                connection.Open();
+                int count = (int)command.ExecuteScalar();
+
+                return count > 0;
+            }
+        }
+    }
+}
